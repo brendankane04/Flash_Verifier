50c4d45d4515b224f9fb61f063503c14a2494c61
Brendan Dunn Kane <brendankane04@gmail.com>
2021-05-19 20:46:23 -0400

    The gpio interrupt was added, but currently empty.

--- Flash_Verifier/gpio.cpp
+++ Flash_Verifier/gpio.cpp
@@ -11,15 +11,32 @@
 
 #include <stdio.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 void GPIO_init()
 {
+	//Initialize the port for blinking as output
 	DDRB |= _BV(PORTB0);
+
+	//Enable the GPIO pin-change interrupt
+	PCMSK1 |= _BV(PCINT13);
+	PCICR |= _BV(PCIE1);
+	DDRC &= ~_BV(PORTC5);
 }
 
 void blink()
 {
 	PORTB = ~PORTB;
 	_delay_ms(100);
+}
+
+//GPIO interrupt which sends over the global count to the flash device
+ISR(PCINT13_vect)
+{
+	//Run only when the signal changes to high
+	if(PORTC & _BV(PORTC5))
+	{
+
+	}
 }
