7b255837acb162b87525562d592527f43f89aa63
Brendan Dunn Kane <brendankane04@gmail.com>
2021-05-31 21:29:08 -0400

    A constructor was added to Timer.

--- Flash_Verifier/timer.h
+++ Flash_Verifier/timer.h
@@ -8,14 +8,19 @@
 #ifndef TIMER_H_
 #define TIMER_H_
 
+#define TIM0_DEV 0
+#define TIM1_DEV 1
+
 namespace Flash_Verifier
 {
 	class Timer
 	{
+		char type;
 		unsigned char count;
 	
 		public:
-			void init();
+			Timer();
+			Timer(char type);
 			void disable();
 			void enable();
 			void inc_count();
